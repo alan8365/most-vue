408712b6183bd27be5e594d25d2f0c5ccd4275a5
alan <alan8365@gmail.com>
2021-10-03 23:27:05 +0800

    add two ppt file

--- src/stories/Upload.vue
+++ src/stories/Upload.vue
@@ -6,32 +6,14 @@
     >
       <template #bottom>
         <div class="row">
-          <div class="col-3">
-            <a
-              :href="`${publicPath}${summaryName}`"
-              target="_blank"
-              style="width: 100%"
-            >
-              <button
-                class="btn most-link-button"
-                v-text="'摘要格式下載'"
-              />
-            </a>
-          </div>
-
-          <div class="col-3">
-            <a
-              :href="`${publicPath}${reportName}`"
-              target="_blank"
-              style="width: 100%"
-            >
-              <button
-                class="btn most-link-button"
-              >
-                精簡報告格式下載
-                <p style="font-size: 14px">(精簡報告撰寫請以10頁內容為限)</p>
-              </button>
-            </a>
+          <div
+            v-for="link in links"
+            :key="link.id"
+            class="col-3"
+          >
+            <most-upload-link
+              v-bind="link"
+            />
           </div>
         </div>
 
@@ -46,9 +28,9 @@
               >
                 <font-awesome-icon
                   icon="cloud-upload-alt"
-                  style="font-size: 36px"
+                  style="font-size: 40px"
                 />
-                <p>
+                <p style="font-size: 26px">
                   點此進入上傳資料
                 </p>
               </button>
@@ -62,6 +44,18 @@
       class="d-block d-sm-none"
     >
       <template #bottom>
+        <div class="row">
+          <div
+            v-for="link in links"
+            :key="link.id"
+            class="col-12 mb-4"
+          >
+            <most-upload-link
+              v-bind="link"
+            />
+          </div>
+        </div>
+
         <div class="col-12">
           <a
             target="_blank"
@@ -83,12 +77,14 @@ import './upload.css';
 import {reactive} from 'vue';
 
 import MostInfo from './Info';
+import MostUploadLink from './UploadLink';
 
 export default {
   name: 'MostUpload',
 
   components: {
     MostInfo,
+    MostUploadLink,
   },
 
   props: {
@@ -102,13 +98,36 @@ export default {
 
   setup(props, {emit}) {
     props = reactive(props);
+
+    const publicPath = process.env.BASE_URL;
+
     return {
       onClick() {
         emit('click');
       },
-      publicPath: process.env.BASE_URL,
-      summaryName: '摘要_議程編號.docx',
-      reportName: '精簡報告_議程編號.docx',
+      links: [
+        {
+          href: `${publicPath}摘要_議程編號.docx`,
+          buttonText: '摘要格式下載',
+        },
+        {
+          href: `${publicPath}精簡報告_議程編號.docx`,
+          buttonText: '精簡報告格式下載',
+          buttonComment: '(精簡報告撰寫請以10頁內容為限)',
+        },
+        {
+          href: `${publicPath}109應科學門海報範本.pptx`,
+          buttonText: '海報範本下載',
+          backgroundColor: '#FFDB5F',
+          fontColor: '#0B0D0C',
+        },
+        {
+          href: `${publicPath}109應科學門簡報範本.pptx`,
+          buttonText: '簡報格式下載',
+          backgroundColor: '#FFDB5F',
+          fontColor: '#0B0D0C',
+        },
+      ],
     };
   },
 };
